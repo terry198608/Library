5e1093fbb0dbfb4a697c1408761579827fc9e01e
yangtianxiang <yang.tianxiang@monstar-lab.com>
2013-06-03 10:20:05 +0800

    socket

---
+++ Socket/socket_client.php
@@ -0,0 +1,40 @@
+<?php
+$fp = fsockopen("219.141.185.227", 10024, $errno, $errstr, 10);
+if($fp) echo 'success123';
+exit;
+//$address = '192.168.10.170';
+$address = '219.141.185.227';
+$port = 10024;
+
+/* Create a TCP/IP socket. */
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+} else {
+    echo "OK.\n";
+}
+    //echo "Attempting to connect to '$address' on port '$service_port'...";
+$result = socket_connect($socket, $address, $port);
+if ($result === false) {
+    echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+} else {
+    echo "OK.\n";
+}
+
+$in = "HEAD / HTTP/1.1\r\n";
+$in .= "Host: www.example.com\r\n";
+$in .= "Connection: Close\r\n\r\n";
+$out = '';
+
+echo "Sending HTTP HEAD request...";
+socket_write($socket, $in, strlen($in));
+echo "OK.\n";
+
+echo "Reading response:\n\n";
+while ($out = socket_read($socket, 2048)) {
+    echo $out;
+}
+
+echo "Closing socket...";
+socket_close($socket);
+echo "OK.\n\n";
