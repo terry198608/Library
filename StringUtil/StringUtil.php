a0070a0fa5017573827b4df466a96d453850acef
yangtianxiang <yang.tianxiang@monstar-lab.com>
2015-01-22 09:23:37 +0800

    StringUtil/StringUtil.php

--- StringUtil/StringUtil.php
+++ StringUtil/StringUtil.php
@@ -74,5 +74,63 @@ class StringUtil
         preg_match_all($pa, $string, $t_string); if(count($t_string[0]) - $start > $sublen) return join('', array_slice($t_string[0], $start, $sublen));
         return join('', array_slice($t_string[0], $start, $sublen));
     } 
+// 计算中文字符串长度
+function utf8_strlen($string = null) {
+    // 将字符串分解为单元
+    preg_match_all('/./us', $string, $match);
+    // 返回单元个数
+    return count($match[0]);
+}
+/*
+* 功能: 作用跟substr一样，除了它不会造成乱码
+* 参数:
+* 返回:
+*/
+function utf8_substr( $str , $start , $length=null ){
+// 先正常截取一遍.
+$res = substr( $str , $start , $length );
+$strlen = strlen( $str );
+/* 接着判断头尾各6字节是否完整(不残缺) */
+// 如果参数start是正数
+if ( $start >= 0 ){
+// 往前再截取大约6字节
+$next_start = $start + $length; // 初始位置
+$next_len = $next_start + 6 <= $strlen ? 6 : $strlen - $next_start;
+$next_segm = substr( $str , $next_start , $next_len );
+// 如果第1字节就不是 完整字符的首字节, 再往后截取大约6字节
+$prev_start = $start - 6 > 0 ? $start - 6 : 0;
+$prev_segm = substr( $str , $prev_start , $start - $prev_start );
+}
+// start是负数
+else{
+// 往前再截取大约6字节
+$next_start = $strlen + $start + $length; // 初始位置
+$next_len = $next_start + 6 <= $strlen ? 6 : $strlen - $next_start;
+$next_segm = substr( $str , $next_start , $next_len );
+// 如果第1字节就不是 完整字符的首字节, 再往后截取大约6字节.
+$start = $strlen + $start;
+$prev_start = $start - 6 > 0 ? $start - 6 : 0;
+$prev_segm = substr( $str , $prev_start , $start - $prev_start );
+}
+// 判断前6字节是否符合utf8规则
+if ( preg_match( '@^([\x80-\xBF]{0,5})[\xC0-\xFD]?@' , $next_segm , $bytes ) ){
+if ( !empty( $bytes[1] ) ){
+$bytes = $bytes[1];
+$res .= $bytes;
+}
+}
+// 判断后6字节是否符合utf8规则
+$ord0 = ord( $res[0] );
+if ( 128 <= $ord0 && 191 >= $ord0 ){
+// 往后截取 , 并加在res的前面.
+if ( preg_match( '@[\xC0-\xFD][\x80-\xBF]{0,5}$@' , $prev_segm , $bytes ) ){
+if ( !empty( $bytes[0] ) ){
+$bytes = $bytes[0];
+$res = $bytes . $res;
+}
+}
+}
+return $res;
+}
     
 }
